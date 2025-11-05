636dcc8237e6dda177d9b632287bbb0ff499f95f
deaneye <118319051+deaneye@users.noreply.github.com>
2025-11-05 22:42:08 +0800

    P3

--- main.ts
+++ main.ts
@@ -1,53 +1,90 @@
+function show_time () {
+    if (Count < 10) {
+        LumexLDM6432.LDM_putString(
+        "0" + convertToText(Count),
+        LumexLDM6432.fontSize.bigSize,
+        0,
+        5,
+        62
+        )
+        LumexLDM6432.LDM_putString(
+        "00",
+        LumexLDM6432.fontSize.bigSize,
+        2,
+        5,
+        62
+        )
+    } else {
+        LumexLDM6432.LDM_putString(
+        convertToText(Count),
+        LumexLDM6432.fontSize.bigSize,
+        0,
+        5,
+        62
+        )
+        LumexLDM6432.LDM_putString(
+        "00",
+        LumexLDM6432.fontSize.bigSize,
+        2,
+        5,
+        62
+        )
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    strip.setPixelColor(0, neopixel.rgb(255, 255, 255))
-    strip.show()
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.show()
+	
 })
 input.onButtonPressed(Button.B, function () {
-    strip.setPixelColor(1, neopixel.rgb(100, 100, 100))
-    strip.show()
-    LumexLDM6432.LDM_putString(
-    "5678",
-    LumexLDM6432.fontSize.smallSize,
-    0,
-    0,
-    54
-    )
+	
 })
-let strip: neopixel.Strip = null
+let Count = 0
+led.enable(false)
+pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
 LumexLDM6432.LDM_setSerial(SerialPin.P0, SerialPin.P1)
 LumexLDM6432.LDM_on()
-LumexLDM6432.LDM_setBrightness(2)
-LumexLDM6432.LDM_playPage1(1)
-strip = neopixel.create(DigitalPin.P2, 60, NeoPixelMode.RGB)
-strip.setBrightness(3)
+LumexLDM6432.LDM_setBrightness(1)
+LumexLDM6432.LDM_playPage1(0)
+let strip = neopixel.create(DigitalPin.P2, 60, NeoPixelMode.RGB)
+strip.setBrightness(60)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.showRainbow(1, 360)
-let Count = 0
-basic.forever(function () {
+Count = 0
+while (pins.digitalReadPin(DigitalPin.P3) == 1) {
     strip.rotate(1)
     strip.show()
     basic.pause(100)
-})
+}
+LumexLDM6432.LDM_playPage1(6)
+strip.showColor(neopixel.rgb(0, 0, 0))
 basic.forever(function () {
     Count += 1
-    basic.showNumber(Count)
     if (Count >= 24) {
         Count = 0
     }
-    if (Count > 6 && Count <= 9) {
+    if (Count == 6) {
         LumexLDM6432.LDM_playPage1(1)
-    } else if (Count > 9 && Count <= 12) {
+        strip.showColor(neopixel.rgb(255, 120, 30))
+    } else if (Count == 9) {
         LumexLDM6432.LDM_playPage1(2)
-    } else if (Count > 12 && Count <= 15) {
+        strip.showColor(neopixel.rgb(255, 200, 100))
+    } else if (Count == 12) {
         LumexLDM6432.LDM_playPage1(3)
-    } else if (Count > 15 && Count <= 18) {
+        strip.showColor(neopixel.rgb(255, 255, 255))
+    } else if (Count == 15) {
         LumexLDM6432.LDM_playPage1(4)
-    } else {
+        strip.showColor(neopixel.rgb(255, 220, 120))
+    } else if (Count == 18) {
+        LumexLDM6432.LDM_playPage1(5)
+        strip.showColor(neopixel.rgb(255, 100, 20))
+    } else if (Count == 21) {
         LumexLDM6432.LDM_playPage1(6)
+        strip.showColor(neopixel.rgb(0, 0, 0))
+    } else {
+    	
     }
+    show_time()
     basic.pause(1000)
 })
